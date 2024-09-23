1d661a5e697018a557f1d937b64f08fac87724b7
Gideon0017 <159183494+Gideon0017@users.noreply.github.com>
2024-09-23 15:02:33 +0100

    added adjustment

--- script.js
+++ script.js
@@ -8,12 +8,16 @@ const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 const volume = document.getElementById('volume');
+const shuffle = document.getElementById('shuffle');
+const repeat = document.getElementById('repeat');
 
 // Song titles
 const songs = ['Peaches - Justin Bieber','Need me - Fireboy DML','Stronger - Young John','Big Big Things - Young John', 'Friendzone - Adekunle Gold', 'hey', 'Summer', 'Ukulele'];
 
 // Keep track of song
-let songIndex = 1;
+let songIndex = 4;
+let isShuffled = false;
+let isRepeating = false;
 
 // Initially load song details
 loadSong(songs[songIndex]);
@@ -41,6 +45,7 @@ function pauseSong() {
   playBtn.querySelector('i.fas').classList.remove('fa-pause');
   audio.pause();
 }
+
 
 function prevSong() {
   songIndex--;
@@ -79,6 +84,16 @@ function setProgress(e) {
 
 }
 
+function shuffleSongs(){
+  if (isShuffled) {
+    for (let i = songs.length - 1; i > 0; i--){
+      const j = Math.floor(Math.random() * (i + 1));
+      [songs[i], songs[j]] = [songs[j], songs[i]];
+    }
+  }
+  console.log(songs)
+}
+
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
 
@@ -93,10 +108,32 @@ prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
 audio.addEventListener('timeupdate', updateProgress);
-audio.addEventListener('ended', () => {
-  songIndex = (songIndex + 1) % songs.length;
-  loadSong(songs[songIndex]);
-  audio.play();
-});
+
 progressContainer.addEventListener('click', setProgress);
+
+shuffle.addEventListener('click', () => {
+  isShuffled = !isShuffled;
+  shuffle.classList.toggle('active', isShuffled);
+  shuffleSongs();
+});
+
+repeat.addEventListener('click', () => {
+  isRepeating = !isRepeating;
+  repeat.classList.toggle('active', isRepeating);
+  // console.log(isRepeating ? 'ON' : 'OFF')
+});
+
+audio.addEventListener('ended', () => {
+  if (isRepeating) {
+    audio.currentTime = 0;
+    audio.play();
+  } else {
+    songIndex = (songIndex + 1) % songs.length;
+    loadSong(songs[songIndex]);
+    audio.play();
+  }
+});
+// audio.addEventListener('ended', () => {
+  
+// })
 volume.addEventListener('change', () => (audio.volume = volume.value));
